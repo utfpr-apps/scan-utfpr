3b158d4a80824cc39b4fbdaa117e9d5b2bb9c38d
Reinaldo Hassel Neto <reinaldohasseln@gmail.com>
2022-03-29 20:18:49 -0300

    Realizada alterações para deploy heroku

--- backend/Utfpr.Api.Scan/Utfpr.Api.Scan/Configuration/AuthenticationConfiguration.cs
+++ backend/Utfpr.Api.Scan/Utfpr.Api.Scan/Configuration/AuthenticationConfiguration.cs
@@ -4,18 +4,13 @@ namespace Utfpr.Api.Scan.Configuration;
 
 public static class AuthenticationConfiguration
 {
-    public static IServiceCollection AddAuthenticationConfiguration(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddAuthenticationConfiguration(this IServiceCollection services)
     {
         services.AddAuthentication(opt =>
-            {
-                opt.DefaultAuthenticateScheme = GoogleDefaults.AuthenticationScheme;
-                opt.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
-            })
-            .AddGoogle(opt =>
-            {
-                opt.ClientId = "";
-                opt.ClientSecret = "";
-            });
+        {
+            opt.DefaultAuthenticateScheme = GoogleDefaults.AuthenticationScheme;
+            opt.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+        });
 
         return services;
     }
---
+++ backend/Utfpr.Api.Scan/Utfpr.Api.Scan/Configuration/MigrateConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Utfpr.Api.Scan.Configuration;
+
+public static class MigrateConfiguration
+{
+    public static WebApplication MigrateDatabase<T>(this WebApplication app) where T : DbContext
+    {
+        using(var scope = app.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            try
+            {
+                var db = services.GetRequiredService<T>();
+                db.Database.Migrate();
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred while migrating the database.");
+            }
+        }
+        return app;
+    }
+}
