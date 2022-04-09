85f5cabee832ef8efbfff466d28379bf95bd8336
Felipe Maccari <felipecesarmaccari@gmail.com>
2022-04-09 00:04:26 -0300

    feat(pages): move font imports to _document

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,21 +1,12 @@
 import Head from "next/head";
+
 import LayoutDashboard from "../layouts/dashboard/LayoutDashboard";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <title>SCAN UTFPR - Pato Branco</title>
+        <title>Dashboard - SCAN UTFPR</title>
       </Head>
 
       <LayoutDashboard />
