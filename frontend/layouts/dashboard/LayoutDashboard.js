11949be31b0881a454976d78415d85fbadbbac7c
Felipe Maccari <felipecesarmaccari@gmail.com>
2022-04-09 00:03:31 -0300

    feat(layouts): add PageLayout to dashboard

--- frontend/layouts/dashboard/LayoutDashboard.js
+++ frontend/layouts/dashboard/LayoutDashboard.js
@@ -1,23 +1,15 @@
-import { Flex } from "@chakra-ui/react";
+import PageLayout from "components/PageLayout";
 
-import PageTitle from "components/PageTitle";
 import LayoutDashboardNotificationList from "./LayoutDashboardNotificationList";
 import LayoutDashboardResume from "./LayoutDashboardResume";
 
 const LayoutDashboard = () => {
   return (
-    <Flex
-      width="100%"
-      px="30px"
-      direction="column"
-      minHeight="calc(100vh - 93px)"
-    >
-      <PageTitle>Página Inicial</PageTitle>
-
+    <PageLayout pageTitle="Página Inicial">
       <LayoutDashboardResume />
 
       <LayoutDashboardNotificationList />
-    </Flex>
+    </PageLayout>
   );
 };
 
