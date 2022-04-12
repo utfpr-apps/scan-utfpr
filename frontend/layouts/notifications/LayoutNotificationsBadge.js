d867b517c436e5bd4cfe58cdc6e5f2a037a41125
Felipe Maccari <felipecesarmaccari@gmail.com>
2022-04-11 23:34:04 -0300

    feat(notifications): add notifications structure

---
+++ frontend/layouts/notifications/LayoutNotificationsBadge.js
@@ -0,0 +1,19 @@
+import { Badge } from "@chakra-ui/react";
+
+const LayoutNotificationsBadge = ({ status }) => {
+  if (status === 0) {
+    return <Badge colorScheme="green">FINALIZADA</Badge>;
+  }
+
+  if (status === 1) {
+    return <Badge colorScheme="yellow">EM ABERTO</Badge>;
+  }
+
+  if (status === 2) {
+    return <Badge colorScheme="red">NÃO NOTIFICADO</Badge>;
+  }
+
+  return <Badge>NÃO DEFINIDO</Badge>;
+};
+
+export default LayoutNotificationsBadge;
---
+++ frontend/layouts/notifications/LayoutNotificationsTable.js
@@ -0,0 +1,111 @@
+import Image from "next/image";
+
+import {
+  Box,
+  Text,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  Button,
+  Flex,
+} from "@chakra-ui/react";
+
+import { BiDotsVerticalRounded } from "react-icons/bi";
+
+import LayoutNotificationsBadge from "./LayoutNotificationsBadge";
+
+const notifications = [
+  {
+    student: "Felipe Maccari",
+    room: "V105",
+    date: "14/Março/2022",
+    dateLimit: "14/Março/2022",
+    status: 0,
+  },
+  {
+    student: "Felipe Maccari",
+    room: "V105",
+    date: "14/Março/2022",
+    dateLimit: "14/Março/2022",
+    status: 1,
+  },
+  {
+    student: "Felipe Maccari",
+    room: "V105",
+    date: "14/Março/2022",
+    dateLimit: "14/Março/2022",
+    status: 2,
+  },
+  {
+    student: "Felipe Maccari",
+    room: "V105",
+    date: "14/Março/2022",
+    dateLimit: "14/Março/2022",
+    status: 0,
+  },
+];
+
+const LayoutNotificationsTable = () => (
+  <TableContainer mt="60px">
+    <Table variant="simple">
+      <Thead>
+        <Tr>
+          <Th>
+            <Text color="greyText" fontSize="18px" fontWeight="semiBold">
+              Nome do Aluno
+            </Text>
+          </Th>
+
+          <Th>
+            <Text color="greyText" fontSize="18px" fontWeight="semiBold">
+              Sala
+            </Text>
+          </Th>
+
+          <Th>
+            <Text color="greyText" fontSize="18px" fontWeight="semiBold">
+              Data Diagnóstico
+            </Text>
+          </Th>
+
+          <Th>
+            <Text color="greyText" fontSize="18px" fontWeight="semiBold">
+              Prazo Atestado
+            </Text>
+          </Th>
+
+          <Th>
+            <Text color="greyText" fontSize="18px" fontWeight="semiBold">
+              Status
+            </Text>
+          </Th>
+        </Tr>
+      </Thead>
+
+      <Tbody>
+        {notifications.map((notification, index) => (
+          <Tr key={index}>
+            <Td>{notification.student}</Td>
+            <Td>{notification.room}</Td>
+            <Td>{notification.date}</Td>
+            <Td>{notification.dateLimit}</Td>
+            <Td>
+              <LayoutNotificationsBadge status={notification.status} />
+            </Td>
+            <Td isNumeric>
+              <Button variant="ghost">
+                <BiDotsVerticalRounded />
+              </Button>
+            </Td>
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
+  </TableContainer>
+);
+
+export default LayoutNotificationsTable;
