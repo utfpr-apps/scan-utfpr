0d885b4e48bc77eef598865b26ee00f0c7c60fd5
Felipe Maccari <felipecesarmaccari@gmail.com>
2022-04-12 22:02:20 -0300

    feat(notifications): add checkbox to change status

--- frontend/layouts/notifications/LayoutNotificationsTableActionsStudents.js
+++ frontend/layouts/notifications/LayoutNotificationsTableActionsStudents.js
@@ -12,6 +12,7 @@ import {
   Flex,
   Text,
   useToast,
+  Checkbox,
 } from "@chakra-ui/react";
 
 const students = [
@@ -69,7 +70,7 @@ const LayoutNotificationsTableActionsStudents = ({ notification }) => {
                 textAlign="center"
               >{`Data limite do atestado: ${notification.dateLimit}`}</Text>
 
-              <Text mt="20px">Os alunos envolvidos nessa notificação são:</Text>
+              <Text mt="50px">Os alunos envolvidos nessa notificação são:</Text>
 
               <Flex direction="column" ml="30px" mt="15px">
                 {students.map((student, index) => (
@@ -81,10 +82,19 @@ const LayoutNotificationsTableActionsStudents = ({ notification }) => {
                   </Flex>
                 ))}
               </Flex>
+
+              <Checkbox defaultChecked mt="50px" ml="25px">
+                Alterar o status da notificação para{" "}
+                <strong>Em Andamento</strong>
+              </Checkbox>
             </Flex>
           </ModalBody>
 
-          <ModalFooter mt="20px">
+          <ModalFooter mt="20px" justifyContent="space-between">
+            <Button variant="ghost" onClick={onClose}>
+              Fechar
+            </Button>
+
             <Button
               variant="outline"
               background="primary"
@@ -92,10 +102,6 @@ const LayoutNotificationsTableActionsStudents = ({ notification }) => {
               onClick={handleCopyEmails}
             >
               Copiar emails
-            </Button>
-
-            <Button variant="ghost" onClick={onClose}>
-              Fechar
             </Button>
           </ModalFooter>
         </ModalContent>
