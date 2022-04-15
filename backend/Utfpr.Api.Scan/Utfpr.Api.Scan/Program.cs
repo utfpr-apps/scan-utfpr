e97dfedcf1cc014a4820597e5eb7c98a0d55bded
Reinaldo Hassel Neto <reinaldohasseln@gmail.com>
2022-04-14 22:21:12 -0300

    Adicionada configuração da connection string com base no environment executado

--- backend/Utfpr.Api.Scan/Utfpr.Api.Scan/Program.cs
+++ backend/Utfpr.Api.Scan/Utfpr.Api.Scan/Program.cs
@@ -2,11 +2,18 @@ using Utfpr.Api.Scan.Configuration;
 using Utfpr.Api.Scan.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var configurationBuilder = new ConfigurationBuilder()
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+    .AddEnvironmentVariables();
+
+builder.Configuration.AddConfiguration(configurationBuilder.Build());
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.ConfigureDatabase(builder.Configuration);
+builder.Services.ConfigureDatabase(builder.Configuration, builder.Environment);
 builder.Services.AddAuthenticationConfiguration();
 builder.Services.AdicionaDependencias();
 builder.Services.AdicionaCors();
