66c4e8d73396ff434157f153f6dbcc73c07e2664
Felipe Maccari <felipecesarmaccari@gmail.com>
2022-06-23 01:25:17 -0300

    chore(core): fix envs

--- frontend/pages/api/auth/[...nextauth].js
+++ frontend/pages/api/auth/[...nextauth].js
@@ -4,9 +4,8 @@ import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId:
-        "1095175628672-ntpftasivik2miab3e8f068che204p0v.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_KEY,
     }),
   ],
 });
