e59eded8ad92559cc52e96f6bd2f5bd530032f5c
Felipe Maccari <felipecesarmaccari@gmail.com>
2022-06-28 19:57:06 -0300

    fix(qrcode): add fallback to qrcode value

--- frontend/pages/qrcode/[value].js
+++ frontend/pages/qrcode/[value].js
@@ -5,11 +5,9 @@ import QRCode from "react-qr-code";
 const QRCodeValue = () => {
   const router = useRouter();
 
-  console.log(router.query.value);
-
   return (
     <Flex height="100vh" width="100%" justify="center" align="center">
-      <QRCode value={router.query.value} />
+      <QRCode value={router.query.value || ""} />
     </Flex>
   );
 };
