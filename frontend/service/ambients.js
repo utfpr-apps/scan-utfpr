429a4b45f21d4775d43ba89b6a402c6ea7851fa8
Felipe Maccari <felipecesarmaccari@gmail.com>
2022-07-05 01:00:01 -0300

    feat(auth): add auth validation

--- frontend/service/ambients.js
+++ frontend/service/ambients.js
@@ -1,39 +1,50 @@
-import axios from "axios";
-
 import { useQuery, useMutation } from "react-query";
+import getApi from "service";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
 
-export const useQueryListAmbients = (options = {}) =>
-  useQuery(
+export const useQueryListAmbients = (options = {}) => {
+  const api = getApi();
+
+  return useQuery(
     ["queryListAmbients"],
-    () => axios.get(`${BASE_URL}/ambientes`).then((result) => result.data),
+    () => api.get(`${BASE_URL}/ambientes`).then((result) => result.data),
     options
   );
+};
 
-export const useMutationCreateAmbients = (options = {}) =>
-  useMutation(
+export const useMutationCreateAmbients = (options = {}) => {
+  const api = getApi();
+
+  return useMutation(
     (ambientData) =>
-      axios
+      api
         .post(`${BASE_URL}/ambientes`, ambientData)
         .then((result) => result.data),
     options
   );
+};
 
-export const useMutationRemoveAmbients = (options = {}) =>
-  useMutation(
+export const useMutationRemoveAmbients = (options = {}) => {
+  const api = getApi();
+
+  return useMutation(
     (ambientId) =>
-      axios
+      api
         .delete(`${BASE_URL}/ambientes/${ambientId}`)
         .then((result) => result.data),
     options
   );
+};
 
-export const useMutationEditAmbients = (options = {}) =>
-  useMutation(
+export const useMutationEditAmbients = (options = {}) => {
+  const api = getApi();
+
+  return useMutation(
     (ambientData) =>
-      axios
+      api
         .put(`${BASE_URL}/ambientes/${ambientData.id}`, ambientData)
         .then((result) => result.data),
     options
   );
+};
--- frontend/service/users.js
+++ frontend/service/users.js
@@ -1,28 +1,36 @@
-import axios from "axios";
-
 import { useQuery, useMutation } from "react-query";
+import getApi from "service";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
 
-export const useQueryListUsers = (options = {}) =>
-  useQuery(
+export const useQueryListUsers = (options = {}) => {
+  const api = getApi();
+
+  return useQuery(
     ["queryListUsers"],
-    () => axios.get(`${BASE_URL}/auth`).then((result) => result.data),
+    () => api.get(`${BASE_URL}/auth`).then((result) => result.data),
     options
   );
+};
 
-export const useMutationCreateUser = (options = {}) =>
-  useMutation(
+export const useMutationCreateUser = (options = {}) => {
+  const api = getApi();
+
+  return useMutation(
     (userData) =>
-      axios
+      api
         .post(`${BASE_URL}/auth/criar-usuario-admin`, userData)
         .then((result) => result.data),
     options
   );
+};
 
-export const useMutationRemoveUser = (options = {}) =>
-  useMutation(
+export const useMutationRemoveUser = (options = {}) => {
+  const api = getApi();
+
+  return useMutation(
     (userId) =>
-      axios.delete(`${BASE_URL}/auth/${userId}`).then((result) => result.data),
+      api.delete(`${BASE_URL}/auth/${userId}`).then((result) => result.data),
     options
   );
+};
